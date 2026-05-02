a14c4cddfafc5fef8b6bd33e031470c3174f4fd2
Roni Silva <ronysiilvaa1@gmail.com>
2026-05-02 16:21:49 -0400

    Forcar backend no deploy principal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'carvion-financeiro-2026-05-02-neon-sync-v22';
+const CACHE_NAME = 'carvion-financeiro-2026-05-02-neon-sync-v23';
 const CORE_ASSETS = [
   './',
   './index.html',
